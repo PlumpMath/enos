0752afda8a06efcaa61dcee7da72285f5f0e0bca
Chris Perkins <cperkins@rallydev.com>
2014-02-19 11:32:43 -0700

    Fix source-paths.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :url "http://github.com/grammati/enos"
   :license "MIT License"
 
-  :source-paths ["src" "dev"]
-  
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
 
   :profiles
   {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
-                        [org.clojure/tools.trace "0.7.6"]]}}
+                        [org.clojure/tools.trace "0.7.6"]]
+         :source-paths ["src" "dev"]
+         }}
   )
