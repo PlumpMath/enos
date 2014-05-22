5f2fa5726ab033d86777b7e691a136f8617297c9
Chris Perkins <cperkins@rallydev.com>
2014-05-22 06:42:55 -0600

    Bump

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject enos "0.1.2"
+(defproject enos "0.1.3-SNAPSHOT"
   
   :description "Clojure core.async utilities"
   :url "http://github.com/grammati/enos"
   :license "MIT License"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]]
 
   :profiles
   {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
